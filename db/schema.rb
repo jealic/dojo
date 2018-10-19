f9a309590dc14e66652507a772c500748dfc0e38
Jessie Ling <jealic@gmail.com>
2018-10-20 02:56:54 +0800

    g model and migrated for categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181019185339) do
+ActiveRecord::Schema.define(version: 20181019185546) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
