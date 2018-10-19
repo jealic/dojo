bde21563497594dc427cd9ef0a0bbe2c8c5bed73
Jessie Ling <jealic@gmail.com>
2018-10-20 03:03:55 +0800

    g model and migrated for post_categoryships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181019185852) do
+ActiveRecord::Schema.define(version: 20181019190225) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 20181019185852) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id"
+  end
+
+  create_table "post_categoryships", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_post_categoryships_on_category_id"
+    t.index ["post_id"], name: "index_post_categoryships_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
