1a5cec30fe8f49c22efe974dddfd692b2a2d1f20
Jessie Ling <jealic@gmail.com>
2018-10-20 02:55:12 +0800

    schema for reply model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181019184213) do
+ActiveRecord::Schema.define(version: 20181019185339) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20181019184213) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "replies", force: :cascade do |t|
+    t.text "comment", null: false
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_replies_on_post_id"
+    t.index ["user_id"], name: "index_replies_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
