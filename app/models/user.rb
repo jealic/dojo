7087ae090006c87cb2d2f5b276d3dc522d229171
Jessie Ling <jealic@gmail.com>
2018-10-20 16:24:56 +0800

    user's associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :posts
+  has_many :replies
+  has_many :collects
+  # 一個 user 收藏多筆 post，同一個 post 可以被不同的 user 收藏
+  has_many :collected_posts, through: :collects, source: :post
+  has_many :friendships #自關聯，有多個朋友
+  has_many :friends, through: :friendships
+  # 自關聯，要先反轉才可以反過來使用, 指定那個 table: class_name, 本來的索引是 friend_id
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :friender, through: :inverse_friendships, source: :user
+  #friender 是加別人當朋友的人
+
   def admin?
     self.role == "admin"
   end
