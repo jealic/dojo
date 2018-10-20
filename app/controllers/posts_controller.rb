9dfbb70c3273cfaad881bfa236c539c87bfd8d98
Jessie Ling <jealic@gmail.com>
2018-10-21 02:30:31 +0800

    set up general resources for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,13 @@
 class PostsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
     @posts = Post.page(params[:page]).per(10)
+    @categories = Category.all
+  end
+
+  def new
+    @post = Post.new
     @categories = Category.all
   end
   
