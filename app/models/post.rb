1c1e6430e3028031f4df67f0102bca69b74e99dd
Jessie Ling <jealic@gmail.com>
2018-10-20 16:37:01 +0800

    post's associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+
+  belongs_to :user, counter_cache: true
+  has_many :collects, dependent: :destroy
+  has_many :replies, dependent: :destroy # post 被刪，其下的 replies 也要被刪，但 user 會在
+  # 一個 post 有多個 categories, 一個 category 屬於多個 post
+  has_many :post_categoryships # 某 post 被刪，跟這個 post 有關的 post-category 記錄也都會沒有
+  has_many :categories, through: :post_categoryships
+  
 end
