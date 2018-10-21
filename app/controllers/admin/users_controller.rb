26e998f323d42704921f605d065b2c0f2e0f9bf0
Jessie Ling <jealic@gmail.com>
2018-10-21 18:25:37 +0800

    admin can update other users as an admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,4 +3,21 @@ class Admin::UsersController < Admin::BaseController
   def index
     @users = User.page(params[:page]).per(10)
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      flash[:notice] = "#{@user.name}'s authority has been updated."
+    else
+      flash[:alert] = @user.errors.full_messages.to_sentence
+    end
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:role)
+  end
 end
