b3bc52d1c31ef279d18f8a522dee0c814e823bf7
Jessie Ling <jealic@gmail.com>
2018-10-21 14:13:05 +0800

    validates uniqueness of category name

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,23 @@
 class Admin::CategoriesController < Admin::BaseController
   def index
-    @categories = Category.all
+    @categories = Category.all 
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category #{@category.name} has been created."
+      redirect_to admin_categories_path
+    else
+      @categories = Category.all
+      render :index
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  
+  validates_uniqueness_of :name
   has_many :post_categoryships
   has_many :posts, through: :post_categoryships, dependent: :restrict_with_error
 end
