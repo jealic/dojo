e93b20b1bc1474a00037bace23c5ead8e11bb928
Jessie Ling <jealic@gmail.com>
2018-10-21 16:38:47 +0800

    case_sensitive: false set category uniqueness in naming

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,14 +1,29 @@
 class Admin::CategoriesController < Admin::BaseController
   def index
     @categories = Category.all 
-    @category = Category.new
+    if params[:id]
+      @category = Category.find(params[:id])
+    else
+      @category = Category.new
+    end
   end
 
   def create
     @category = Category.new(category_params)
     if @category.save
-      flash[:notice] = "Category #{@category.name} has been created."
+      flash[:notice] = "Category #{@category.name.capitalize} has been created."
       redirect_to admin_categories_path
+    else
+      @categories = Category.all
+      render :index
+    end
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to admin_categories_path
+      flash[:notice] = "Category #{@category.name.capitalize} has been updated."
     else
       @categories = Category.all
       render :index
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, case_sensitive: false #不分大小寫
   has_many :post_categoryships
   has_many :posts, through: :post_categoryships, dependent: :restrict_with_error
 end
