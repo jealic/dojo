ddff0c9de9d99c9d4d1be03654920a76c1168040
Jessie Ling <jealic@gmail.com>
2018-10-27 17:06:08 +0800

    user can leave a comment on published post

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,13 +1,30 @@
 class RepliesController < ApplicationController
+  
   before_action :set_post
+
   def create
-    if @post.draft == true
+    if @post.draft == false
+      @reply = @post.replies.new(reply_params)
+      @reply.user = current_user
+      if @reply.save
+        flash[:notice] = "You've commented in article \"#{@post.title}\". "
+      else
+        flash[:alert] = @reply.errors.full_messages.to_sentence if @reply.errors.any?
+      end
+    else
+      flash[:alert] = "You cannot leave your comment here!"
+    end
+    redirect_to post_path(@post)
   end
 
   private
 
   def set_post
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
+  end
+
+  def reply_params
+    params.require(:reply).permit(:comment)
   end
 
 end
