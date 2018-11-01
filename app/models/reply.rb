3c836d3a2fab35dbc55faad2af981b2c0df8d5ea
Jessie Ling <jealic@gmail.com>
2018-11-01 12:16:31 +0800

    set a method to match post.replied_time and reply.comment.time

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,8 @@
 class Reply < ApplicationRecord
   belongs_to :user, counter_cache: true
   belongs_to :post, counter_cache: true
+
+  def match_post_last_replied_at
+    self.post.update(last_replied_at: self.updated_at)
+  end
 end
