216c7f41d0e870a7ae7735f1f5d1cb5b1e03b28e
Jessie Ling <jealic@gmail.com>
2018-11-01 12:38:36 +0800

    set up sorting link on post index page

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -2,6 +2,8 @@ class Reply < ApplicationRecord
   belongs_to :user, counter_cache: true
   belongs_to :post, counter_cache: true
 
+  after_save :match_post_last_replied_at
+
   def match_post_last_replied_at
     self.post.update(last_replied_at: self.updated_at)
   end
