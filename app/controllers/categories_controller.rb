2a97675a302983bf5cdbe270b4c0b1063012500d
Jessie Ling <jealic@gmail.com>
2018-11-02 11:31:35 +0800

    completed category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,9 @@
 class CategoriesController < ApplicationController
+  
+  def show
+    @categories = Category.all
+    @category = Category.find(params[:id])
+    @q = @category.posts.where(draft: false).ransack(params[:q])
+    @posts = @q.result.page(params[:page]).per(20)
+  end
 end
