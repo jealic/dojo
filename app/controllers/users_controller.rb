4ff6fb9c2c9fb72b45678024e1246d04043182ff
Jessie Ling <jealic@gmail.com>
2018-11-03 11:35:06 +0800

    tried to be the same as the example

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,10 +72,10 @@ class UsersController < ApplicationController
   end
 
   def accept_friend
-    @friendship = Friendship.find_by(user: @user, friend: current_user)
+    friendship = Friendship.find_by(user: @user, friend: current_user)
     # 找到被別人邀請的那比資料，也就是 B→A + pending 這筆
-    @friendship.invite = "accpet" # 改寫 invite column 的 string 值
-    if @friendship.save
+    friendship.invite = "accpet" # 改寫 invite column 的 string 值
+    if friendship.save
       @accepted_frienders = current_user.frienders.where('friendships.invite = ?', 'pending')
       # 回去找出寄邀請給我的那比資料的 user (使用 inverse_friendships) || 那比資料的 invite 是 pending 值
       flash[:notice] = "Friendship with #{@user.name} is permitted."
@@ -86,7 +86,7 @@ class UsersController < ApplicationController
   end
 
   def ignore_friend
-    @friendship = Friendship.find_by(user: @user, friend: current_user)
+    friendship = Friendship.find_by(user: @user, friend: current_user)
     friendship.invite = "ignore"
     if friendship.save
       @ignored_frienders = current_user.frienders.where('friendships.invite = ?', 'pending')
@@ -98,15 +98,7 @@ class UsersController < ApplicationController
   end
 
   def show_friend
-    if @user == current_user
-      @waiting_friends = current_user.friends.where.not('friendships.invite = ?', 'accept')
-      @accepted_frienders = current_user.friends.where('friendships.invite = ?', 'accept')
-      @ignored_frienders = current_user.frienders.where('friendships.invite = ?', 'pending')
-      render :show
-    else
-      flash[:alert] = "Have no authority to the content"
-      redirect_back fallback_location: root_path
-    end
+    
   end
 
   private
