11f9d1f3fa937d4cfeac410ca0e3c4166e54b0f8
Jessie Ling <jealic@gmail.com>
2018-11-04 08:54:58 +0800

    show api post.show

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::PostsController < ApiController
   before_action :authenticate_user!, except: :index
+  before_action :set_post, only: [:show, :update, :destroy]
 
   def index
     @categories = Category.all
@@ -23,12 +24,51 @@ class Api::V1::PostsController < ApiController
           title: post.title,
           content: post.content,
           image: post.image,
-          category_ids: post.categories.ids
+          categories: post.categories.map do |c|
+            {
+              name: c.name,
+            }
+          end
         }
       end
     }
   end
 
+  def show
+    if @post.be_viewed_by?(current_user)
+      @reply = Reply.new
+      @replies = @post.replies.order("updated_at DESC").page(params[:page]).per(20)
+      @post.count_views
+      @post.save
+    else
+      flash[:alert] = 'The post is not open to you.'
+      redirect_back fallback_location: root_path
+    end
+    if !@post
+      render json: {
+        message: "can't find the post!",
+        status: 400
+      }
+    else
+      render json: {
+        title: @post.title,
+        content: @post.content,
+        image: @post.image,
+        categories: @post.categories.map do |c|
+          {
+            name: c.name,
+          }
+        end,
+        replies: @post.replies.map do |reply|
+          {
+            user: reply.user.name,
+            comment: reply.comment,
+          }
+        end
+      }
+    end
+    
+  end
 
 
   private
