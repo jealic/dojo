7390fabbcb4686aea1f163871382b41656abea0a
Jessie Ling <jealic@gmail.com>
2018-11-04 10:45:31 +0800

    add/g_authentication_token (user model) and add token to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181103033724) do
+ActiveRecord::Schema.define(version: 20181104024403) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
@@ -89,6 +89,8 @@ ActiveRecord::Schema.define(version: 20181103033724) do
     t.integer "replies_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "authentication_token"
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
